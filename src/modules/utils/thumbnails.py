4abb52d9a3ac2c2e6630cb674344a87f3cf59cd6
𝐆𝐫𝐚𝐲𝐁𝐨𝐭𝐬 <teamstormxd@gmail.com>
2025-05-03 16:33:35 +0530

    Update thumbnails.py

--- src/modules/utils/thumbnails.py
+++ src/modules/utils/thumbnails.py
@@ -1,7 +1,3 @@
-#  Copyright (c) 2025 AshokShau
-#  Licensed under the GNU AGPL v3.0: https://www.gnu.org/licenses/agpl-3.0.html
-#  Part of the TgMusicBot project. All rights reserved where applicable.
-
 import asyncio
 from io import BytesIO
 
@@ -9,8 +5,9 @@ import httpx
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from aiofiles.os import path as aiopath
 
-from src.helpers import CachedTrack
+import config
 from src.logger import LOGGER
+from src.platforms.dataclass import CachedTrack
 
 FONTS = {
     "cfont": ImageFont.truetype("src/modules/utils/cfont.ttf", 15),
@@ -23,7 +20,6 @@ FONTS = {
 def resize_youtube_thumbnail(img: Image.Image) -> Image.Image:
     """
     Resize a YouTube thumbnail to 640x640 while keeping important content.
-
     It crops the center of the image after resizing.
     """
     target_size = 640
@@ -47,63 +43,33 @@ def resize_youtube_thumbnail(img: Image.Image) -> Image.Image:
     return img.crop((left, top, right, bottom))
 
 
-def resize_jiosaavn_thumbnail(img: Image.Image) -> Image.Image:
-    """
-    Resize a JioSaavn thumbnail from 500x500 to 600x600.
-
-    It upscales the image while preserving quality.
-    """
-    target_size = 600
-    img = img.resize((target_size, target_size), Image.Resampling.LANCZOS)
-    return img
-
-
 async def fetch_image(url: str) -> Image.Image | None:
-    """
-    Fetches an image from the given URL, resizes it if necessary for JioSaavn and
-    YouTube thumbnails, and returns the loaded image as a PIL Image object, or None on
-    failure.
-
-    Args:
-        url (str): URL of the image to fetch.
-
-    Returns:
-        Image.Image | None: The fetched and possibly resized image, or None if the fetch fails.
-    """
     if not url:
         return None
 
     async with httpx.AsyncClient() as client:
         try:
-            if url.startswith("https://is1-ssl.mzstatic.com"):
-                url = url.replace("500x500bb.jpg", "600x600bb.jpg")
             response = await client.get(url, timeout=5)
             response.raise_for_status()
             img = Image.open(BytesIO(response.content)).convert("RGBA")
+
             if url.startswith("https://i.ytimg.com"):
                 img = resize_youtube_thumbnail(img)
-            elif url.startswith("http://c.saavncdn.com") or url.startswith(
-                "https://i1.sndcdn"
-            ):
-                img = resize_jiosaavn_thumbnail(img)
+
             return img
         except Exception as e:
-            LOGGER.error("Image loading error: %s", e)
+            LOGGER.error(f"Image loading error: {e}")
             return None
 
 
 def clean_text(text: str, limit: int = 17) -> str:
-    """
-    Sanitizes and truncates text to fit within the limit.
-    """
-    text = text.strip()
-    return f"{text[:limit - 3]}..." if len(text) > limit else text
+    """Sanitizes and truncates text to fit within the limit."""
+    text = text.strip()[:limit]
+    return f"{text}..." if len(text) == limit else text
 
 
 def add_controls(img: Image.Image) -> Image.Image:
-    """
-    Adds blurred background effect and overlay controls.
-    """
+    """Adds blurred background effect and overlay controls."""
     img = img.filter(ImageFilter.GaussianBlur(25))
     box = (120, 120, 520, 480)
 
@@ -123,9 +89,7 @@ def add_controls(img: Image.Image) -> Image.Image:
 
 
 def make_sq(image: Image.Image, size: int = 125) -> Image.Image:
-    """
-    Crops an image into a rounded square.
-    """
+    """Crops an image into a rounded square."""
     width, height = image.size
     side_length = min(width, height)
     crop = image.crop(
@@ -147,9 +111,7 @@ def make_sq(image: Image.Image, size: int = 125) -> Image.Image:
 
 
 def get_duration(duration: int, time: str = "0:24") -> str:
-    """
-    Calculates remaining duration.
-    """
+    """Calculates remaining duration."""
     try:
         m1, s1 = divmod(duration, 60)
         m2, s2 = map(int, time.split(":"))
@@ -157,14 +119,12 @@ def get_duration(duration: int, time: str = "0:24") -> str:
         _min, sec = divmod(sec, 60)
         return f"{_min}:{sec:02d}"
     except Exception as e:
-        LOGGER.error("Duration calculation error: %s", e)
+        LOGGER.error(f"Duration calculation error: {e}")
         return "0:00"
 
 
 async def gen_thumb(song: CachedTrack) -> str:
-    """
-    Generates and saves a thumbnail for the song.
-    """
+    """Generates and saves a thumbnail for the song."""
     save_dir = f"database/photos/{song.track_id}.png"
     if await aiopath.exists(save_dir):
         return save_dir
@@ -174,7 +134,7 @@ async def gen_thumb(song: CachedTrack) -> str:
 
     thumb = await fetch_image(song.thumbnail)
     if not thumb:
-        return ""
+        return config.YOUTUBE_IMG_URL
 
     # Process Image
     bg = add_controls(thumb)
@@ -185,10 +145,10 @@ async def gen_thumb(song: CachedTrack) -> str:
     bg.paste(image, (paste_x, paste_y), image)
 
     draw = ImageDraw.Draw(bg)
-    draw.text((285, 180), "Audify Music", (192, 192, 192), font=FONTS["nfont"])
+    draw.text((285, 180), "Fallen Beatz", (192, 192, 192), font=FONTS["nfont"])
     draw.text((285, 200), title, (255, 255, 255), font=FONTS["tfont"])
     draw.text((287, 235), artist, (255, 255, 255), font=FONTS["cfont"])
     draw.text((478, 321), get_duration(duration), (192, 192, 192), font=FONTS["dfont"])
 
     await asyncio.to_thread(bg.save, save_dir)
-    return save_dir if await aiopath.exists(save_dir) else ""
+    return save_dir if await aiopath.exists(save_dir) else config.YOUTUBE_IMG_URL
